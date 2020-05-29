00895c36946ca57956b334f934c73910b102f974
Kirill Zaidulin <zaidulinkirill@gmail.com>
2020-05-29 10:00:55 +0300

    Initial

--- saofile.js
+++ saofile.js
@@ -1,57 +1,48 @@
-const superb = require('superb')
+const { resolve } = require("path");
+
+const CONFIG_FILE = 'qms.config.json'
 
 module.exports = {
   prompts() {
     return [
-      {
-        name: 'name',
-        message: 'What is the name of the new project',
-        default: this.outFolder,
-        filter: val => val.toLowerCase()
-      },
-      {
-        name: 'description',
-        message: 'How would you descripe the new project',
-        default: `my ${superb()} project`
-      },
-      {
-        name: 'username',
-        message: 'What is your GitHub username',
-        default: this.gitUser.username || this.gitUser.name,
-        filter: val => val.toLowerCase(),
-        store: true
-      },
-      {
-        name: 'email',
-        message: 'What is your email?',
-        default: this.gitUser.email,
-        store: true
-      },
-      {
-        name: 'website',
-        message: 'The URL of your website',
-        default({ username }) {
-          return `github.com/${username}`
-        },
-        store: true
-      }
+      // {
+      //   name: 'name',
+      //   message: 'What is the name of the new project',
+      //   default: this.outFolder,
+      //   filter: val => val.toLowerCase()
+      // }
     ]
   },
-  actions: [
-    {
-      type: 'add',
-      files: '**'
-    },
-    {
-      type: 'move',
-      patterns: {
-        gitignore: '.gitignore'
-      }
+  actions: (generator) => {
+    // generator.spinner.start('Generating Nest.js project')
+
+    return [
+      {
+        type: 'add',
+        files: '**'
+      },
+      // {
+      //   type: 'move',
+      //   patterns: {
+      //     gitignore: '.gitignore'
+      //   }
+      // }
+    ]
+  },
+  async prepare(context) {
+    if (!await this.fs.pathExists(resolve(CONFIG_FILE))) {
+      throw this.createError(`Missing ${CONFIG_FILE}`)
     }
-  ],
+
+    context.$config = JSON.parse(this.fs.readFileSync(resolve(CONFIG_FILE), { encoding: 'utf8' }))
+  },
+  templateData() {
+    return this.$config
+  },
   async completed() {
-    this.gitInit()
-    await this.npmInstall()
-    this.showProjectTips()
+    // console.log(this)
+    // this.gitInit()
+    // await this.npmInstall()
+    // this.showProjectTips()
   }
 }
